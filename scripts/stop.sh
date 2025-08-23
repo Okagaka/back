779d5b954924e51529602d6787262fec6b6efa82
jinsol24 <solsoli2468@gmail.com>
2025-08-23 17:30:39 +0900

    [fix] #3 stop.sh start.sh appspec.yml

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -3,8 +3,10 @@
 # =============================
 # 환경 설정
 # =============================
-PROJECT_ROOT="/home/ubuntu/apps/spring-practice"
-JAR_FILE="$PROJECT_ROOT/spring-webapp.jar"
+#PROJECT_ROOT="/home/ubuntu/apps/spring-practice"
+#JAR_FILE="$PROJECT_ROOT/spring-webapp.jar"
+PROJECT_ROOT="/home/ubuntu/apps/okagaka-server"
+#JAR_FILE="$PROJECT_ROOT/build/libs/okagaka-server-0.0.1-SNAPSHOT.jar"
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 # 디렉터리와 로그 파일 생성
@@ -16,7 +18,8 @@ TIME_NOW=$(date +%c)
 # =============================
 # 현재 구동 중인 애플리케이션 pid 확인
 # =============================
-CURRENT_PID=$(pgrep -f "$JAR_FILE")
+#CURRENT_PID=$(pgrep -f "$JAR_FILE")
+CURRENT_PID=$(pgrep -f "$PROJECT_ROOT/build/libs/.*\.jar")
 
 if [ -z "$CURRENT_PID" ]; then
   echo "$TIME_NOW > 현재 실행 중인 애플리케이션이 없습니다" >> "$DEPLOY_LOG"
